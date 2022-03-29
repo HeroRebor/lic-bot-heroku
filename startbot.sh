1e16907d957ce9071a4ed5ffb4244049f52437e3
HeroRebor <96386317+HeroRebor@users.noreply.github.com>
2022-03-29 16:33:46 +0530

    Update startbot.sh

--- startbot.sh
+++ startbot.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 chmod +x engines/stockfish_14.1_linux_x64_popcnt
 chmod a+x engines/fsf
-python user_interface.py
+
+### ONLY UNCOMMENT ONE START POINT ###
+
+python heroku_challenges.py         # This start point will accept challenges according to the config.yml
+
+# python heroku_matchmaking.py        # This start point will challenge other bots and decline all challenges. CAUTION!!! Don't run this 24/7, you will get rate limited by lichess.
